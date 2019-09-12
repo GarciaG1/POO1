a2b041051f9621d6debb0230a8d465d04f40a35f
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-09-12 08:35:18 -0700

    t2
    t2

--- p1/Program.cs
+++ p1/Program.cs
@@ -2,11 +2,32 @@
 
 namespace p1
 {
+
+class Bici
+{
+    //Campos
+    public string marca;
+    public string color;
+    public string type;
+    public int velocities;
+    public int size;
+
+    //Metodos 
+    public void print(){
+        Console.WriteLine("Marca{0},Color{1}",marca,color);
+    }
+
+}
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Tec");
+            Bici bici = new Bici();
+            bici.marca ="BiciMex";
+            bici.color="Rojo";
+           bici.print();
+            //Console.WriteLine("HolaTec");
         }
     }
 }
+
