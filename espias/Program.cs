99abb0e58fa233a0609ef39984297a6ea14bbb16
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-09-24 14:43:43 -0700

    esp
    trabajo en clase (ocultar nombre)

---
+++ espias/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace espias
+{
+  class persona
+  {
+     public string nombre;
+     public string apellido;
+     public persona()
+     {
+         nombre="fulano";
+         apellido="De Tal";
+     }
+      
+  }
+
+    class anonymous{
+        public static void anonimiza( persona p)
+        {
+            p.nombre= "xxxxxxxxxxx";
+            p.apellido="xxxxxxxxxxxxxx";
+        }
+
+        public static void cambia (ref persona p)
+        {
+            p= new persona ();
+            p.nombre= "new"; 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             persona p=new persona();
+             Console.WriteLine(p.nombre);
+             anonymous.anonimiza(p);
+             anonymous.cambia(ref p);
+             Console.WriteLine(p.nombre);
+             
+             
+           
+        }
+    }
+}
