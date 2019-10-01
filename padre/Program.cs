6e97718cfb43e5d5cb6de8cd03aa9ea4955458a3
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-10-01 01:04:22 -0700

    t.c.

---
+++ padre/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace padre
+{
+    class Componente
+    {
+        public string nombre;
+        public contenedor padre;
+
+        public Componente (string nombre,contenedor padre)
+        {
+            this.nombre=nombre;
+            this.padre= padre;
+        }
+        public void print(){
+            Console.WriteLine({$"Soy (nombre) y mi padre es (padre.nombre)"});
+        }
+    }
+
+    class contenedor{
+        public string nombre;
+        public List<Componente> componentes = new List<Componente>();
+
+        public contenedor(string nombre)
+        {
+            this.nombre = nombre;
+            componentes.Add(new Componente("primero",this));
+            componentes.Add(new Componente("segundo",this));
+            componentes.Add(new Componente("tercero",this));
+        }
+        public void print(){
+            foreach(var hijo in componentes)
+            {
+                hijo.print();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            contenedor padre1= new contenedor("contenedor1");
+            contenedor padre2= new contenedor("contenedor2");
+            Console.WriteLine("-----------------------------------");
+            padre1.print();
+            padre2.print();
+        }
+    }
+}
