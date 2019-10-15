f8f980dc8e32680a030caffd8577aadb47579edb
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-10-14 19:39:03 -0700

    herencia

---
+++ Musico/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Musico
+{
+    class Musico {
+        protected string nombre;
+    
+    public Musico (string n){
+        nombre=n;
+    }
+    public void saluda(){
+        Console.WriteLine("Hola soy:",nombre);
+
+    }
+        public void afina(){
+        Console.WriteLine("{0} afina su instrumento ",nombre);
+    
+
+        }
+
+    }
+
+    class Bajista: Musico{
+     private string bajo;
+     public Bajista (string nom,string bajo):base(nom){
+
+            this.bajo=bajo;
+        }
+        public new void afina(){
+        Console.WriteLine("{0} afina su bajo {1}",nombre,bajo);}
+    
+
+    }
+
+     
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musico tom = new Musico("Tom");
+            Bajista Flea = new Bajista("Flea","Gibson");
+            tom.saluda();
+            Flea.saluda();
+            tom.afina();
+            Flea.afina();
+        }
+    }   
+}   
+
