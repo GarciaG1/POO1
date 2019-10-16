c81c7ccc16d14c8738714d384c5150110899663c
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-10-16 00:22:55 -0700

    m1

--- Musico/Program.cs
+++ Musico/Program.cs
@@ -1,49 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace Musico
 {
-    class Musico {
-        protected string nombre;
+    class Musico 
+    {
+            protected string nombre;
     
-    public Musico (string n){
-        nombre=n;
-    }
-    public void saluda(){
-        Console.WriteLine("Hola soy:",nombre);
+            public Musico (string n)
+            {
+                nombre=n;
+            }
+            public virtual void saluda()
+            {
+            return string.Format ("Hola soy {0}",nombre);
 
-    }
-        public void afina(){
+            }
+        //public void afina(){
+        public virtual void afina ()
+        {
         Console.WriteLine("{0} afina su instrumento ",nombre);
-    
-
         }
+    }                                      
+            class Bajista:Musico
+    {
+        private string bajo;
+        public Bajista (string nom,string b):base(nom)
+        {
 
+            this.bajo=b;
+        }
+    
+        //public new void afina(){
+             public override void  afina()
+             {
+              Console.WriteLine("{0} afina su bajo {1}",nombre,bajo);
+             }
+         
+              public override string saluda()
+               {
+                return base.saluda()+"y soy bajista";
+              }
     }
 
-    class Bajista: Musico{
-     private string bajo;
-     public Bajista (string nom,string bajo):base(nom){
+    class Guitarrista:Musico
+    {
+         private string guitarra;
 
-            this.bajo=bajo;
+         public Guitarrista(string n,string g):base(n)
+         {
+            guitarra=g;
+         }
+
+        public override void afina ()
+        {
+            
+            console.WriteLine("{0} afinando su  {1}",nombre,guitarra);
         }
-        public new void afina(){
-        Console.WriteLine("{0} afina su bajo {1}",nombre,bajo);}
-    
-
-    }
-
-     
- 
+        public override string saluda()
+        {
+            return base.saluda()+"y soy guitarrista";
+        }
+        
+     }
+            
+   
     class Program
     {
         static void Main(string[] args)
         {
-            Musico tom = new Musico("Tom");
-            Bajista Flea = new Bajista("Flea","Gibson");
-            tom.saluda();
-            Flea.saluda();
-            tom.afina();
-            Flea.afina();
+            Musico Gabriel = new Musico("Gabriel");
+            Bajista Garcia = new Bajista("Garcia","Gibson");
+            Guitarrista Angel= new Guitarrista("Angel","x");
+            
+
+            List < Musico > grupo = new List <Musico>();
+            grupo.Add(Gabriel);
+            grupo.Add(Garcia);
+            grupo.Add (Angel);
+            foreach (Musico m in grupo)
+            {
+                console.writeline(m.saluda());
+                m.afina();
+            }
+            
+                
+            
         }
     }   
 }   
