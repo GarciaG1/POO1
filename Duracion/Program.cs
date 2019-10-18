b682b276e8fb9edc2aa59796a0bdb602cd6975cc
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-10-18 13:15:47 -0700

    c4

--- Duracion/Program.cs
+++ Duracion/Program.cs
@@ -2,56 +2,82 @@
 
 namespace Duracion
 {
-    class Duracion 
-{
-    
-    public int horas;
-    public int minutos;
-    public int segundos;
-public Duracion (int h,int m,int s)  
- {
-     this.horas=h;
-     this.minutos=m;
-     this.segundos=s;
- } 
- public Duracion (int s)  
- {
-     
-     
-    this.segundos=s;
- } 
- public void print()
- {
-     Console.WriteLine("Duracion= ["+horas+":"+minutos+":"+segundos+"]");
- }
-  
- public int get_minutos ()
- {
-     int m;
-     m=minutos=(horas*60) + minutos;
-    return m;
-    
- }
-  public int  get_segundos()
- {  int s;
-     s= (minutos*60) + (horas*3600)+segundos;
-   
-     
-    return s;
- }
-}
 
-class Program
+    class Duracion
+    {
+        public int horas;
+        public int minutos;
+        public int segundos;
+
+        public Duracion (int h, int m,int s)
+        {
+            this.horas=h;
+            this.minutos=m;
+            this.segundos=s;
+        }
+        public Duracion (int s)
+        {
+            this.segundos=s;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Duracion=["+horas+":"+minutos+":"+segundos+"]");
+        }
+
+        public void print2()
+        {
+            
+            Console.WriteLine("a+b= "+segundos);
+        }
+
+        
+        public int get_segundos()
+        {
+            int s;
+            s=(minutos*60)+(horas*3600)+segundos;
+
+            return s;
+        }
+        public int get_minutos()
+        {
+            int m;
+            m= minutos=(horas*60)+minutos;
+
+            return m;
+        }
+        public static Duracion operator +(Duracion a, Duracion b)
+        {
+            int sum=0;
+            sum=sum+(a.horas+b.horas)*3600;
+            sum=sum+(a.minutos+b.minutos)*60;
+            sum=sum+(a.segundos+b.segundos);
+
+            return new Duracion (sum);
+
+
+        }
+
+
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-          Duracion Total=new Duracion(03,20,10);
-         
-         
-         Total.print();  
-        Console.WriteLine("segundos:"+Total.get_segundos());
-        Console.WriteLine("minutos:"+Total.get_minutos());
-
+            
+           Duracion Total = new Duracion(03,20,10);
+            Duracion a= new Duracion (5,22,43);
+            Duracion b= new Duracion(07,23,08);
+            Duracion c;
+            
+           
+           Total.print();
+           Console.WriteLine("Minutos:"+Total.get_minutos());
+           Console.WriteLine("Segundos:"+Total.get_segundos());
+            c=a+b;
+           c.print2();
         }
+
     }
 }
+
