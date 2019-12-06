43118187fd6e23a1fca3a4d00e0796a555c6fa6b
GarciaG1 <54416706+GarciaG1@users.noreply.github.com>
2019-12-06 12:25:21 -0800

    stack2

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,34 +1,58 @@
-﻿using System; 
-
+﻿using System;
+using System.Collections.Generic;
 
 namespace Stack
 {
+
     public class Pila <T>
     {
         readonly int tamaño;
                  int apuntador=0;
-                T [] elementos;
+                 T [] elementos;
+
+        
+        
         public Pila (int size)
         {
-            tamaño=size;
-            elementos=new T [tamaño];
-        }
-    
-    public void Push ( T elemento)
-    {
-        if (apuntador <tamaño)
-        elementos[apuntador]=elemento;
-        apuntador++;
+            tamaño = size;
+            elementos =new T [tamaño];
+        } 
+        public void Push ( T elemento )
         {
+            if (apuntador >= tamaño)
+            throw new StackOverflowException();
+            elementos[apuntador]= elemento;
+            apuntador++;
+            {
 
+            }
+        }
+        public T Pop ()
+        {
+       
+            
+			if (apuntador >= 0)
+			{
+				return elementos[apuntador];
+			}
+			else
+			{
+				apuntador= 0;
+				throw new InvalidOperationException("vacio" );
+			}
+        }
+  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> m = new Stack<string>();
+			m.Push("Adios ");
+			m.Push(" , ");
+			m.Push("Hola");
+			
+			Console.WriteLine(m.Pop()+m.Pop()+m.Pop());
+           
         }
     }
-    public T Pop ()
-    {
-        apuntador --;
-        return elementos[apuntador];
-    }
-}
-     
-    
 }
